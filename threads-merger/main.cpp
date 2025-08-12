6725a8a9553995088638605ac0219ed457ff8f8f
Dmitry Bravikov <dmitry@bravikov.ru>
2025-08-12 22:57:50 +0300

    C++ implementation of stacks merging

---
+++ threads-merger/main.cpp
@@ -0,0 +1,162 @@
+#include <time.h>
+#include <gtest/gtest.h>
+#include <unordered_map>
+#include <unordered_set>
+
+import merger;
+
+using namespace std;
+
+enum {
+    A=0,
+    B=1,
+    C=2,
+    D=3,
+    E=4,
+    F=5,
+    G=6,
+};
+
+TEST(merge, two_intersected_stacks)
+{
+    auto input = std::vector<std::vector<int>>{
+        {F, E, D, C, B, A},
+        {F, E, G, C, B, A},
+    };
+
+    Node<int> nodeF1{.count=1, .level=6, .next_nodes={} };
+    Node<int> nodeE1{.count=1, .level=5, .next_nodes={{F, nodeF1},} };
+    Node<int> nodeD {.count=1, .level=4, .next_nodes={{E, nodeE1},} };
+
+    Node<int> nodeF2{.count=1, .level=6, .next_nodes={} };
+    Node<int> nodeE2{.count=1, .level=5, .next_nodes={{F, nodeF2},} };
+    Node<int> nodeG {.count=1, .level=4, .next_nodes={{E, nodeE2},} };
+
+    Node<int> nodeC {.count=2, .level=3, .next_nodes={{D, nodeD}, {G, nodeG}, }};
+    Node<int> nodeB {.count=2, .level=2, .next_nodes={{C, nodeC}, }};
+    Node<int> nodeA {.count=2, .level=1, .next_nodes={{B, nodeB}, }};
+
+    Node<int> root  {.count=2, .level=0, .next_nodes={{A, nodeA}, }};
+
+    const auto& expected = root;
+
+    auto actual = merge<int>(input);
+
+    EXPECT_EQ(actual, expected);
+}
+
+
+TEST(merge, independent_stacks) {
+    auto input = std::vector<std::vector<int>>{
+        {C, B, A},
+        {F, E, D},
+    };
+
+    // Ожидаемый результат: два независимых стека
+    Node<int> nodeC{.count=1, .level=3, .next_nodes={} };
+    Node<int> nodeB{.count=1, .level=2, .next_nodes={{C, nodeC}} };
+    Node<int> nodeA{.count=1, .level=1, .next_nodes={{B, nodeB}} };
+
+    Node<int> nodeF{.count=1, .level=3, .next_nodes={}};
+    Node<int> nodeE{.count=1, .level=2, .next_nodes={{F, nodeF}} };
+    Node<int> nodeD{.count=1, .level=1, .next_nodes={{E, nodeE}} };
+
+    Node<int> root {.count=2, .level=0, .next_nodes={{A, nodeA}, {D, nodeD}}};
+
+    const auto& expected = root;
+    auto actual = merge<int>(input);
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(merge, stack_with_recursion) {
+    auto input = std::vector<std::vector<int>>{
+        {B, A, A, A},
+    };
+
+    // Ожидаемый результат: стек с рекурсией
+    // Одинаковые элементы на одном уровне должны объединяться
+    Node<int> nodeB  {.count=1, .level=4, .next_nodes={} };
+    Node<int> nodeA3{.count=1, .level=3, .next_nodes={{B, nodeB}} };
+    Node<int> nodeA2{.count=1, .level=2, .next_nodes={{A, nodeA3}} };
+    Node<int> nodeA1{.count=1, .level=1, .next_nodes={{A, nodeA2}} };
+
+    Node<int> root{.count=1, .level=0, .next_nodes={{A, nodeA1}} };
+
+    const auto& expected = root;
+    auto actual = merge<int>(input);
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(merge, empty_lists) {
+    // Тест на пустой список стеков
+    auto input = std::vector<std::vector<int>>{};
+
+    Node<int> expected{};
+
+    auto actual = merge<int>(input);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(merge, empty_stacks) {
+    // Тест на список, содержащий только пустые стеки
+    auto input = std::vector<std::vector<int>>{
+        {},
+        {},
+        {}
+    };
+
+    Node<int> expected{};
+
+    auto actual = merge<int>(input);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(merge, mixed_empty_stacks) {
+    // Тест на список, содержащий смесь пустых и непустых стеков
+    auto input = std::vector<std::vector<int>>{
+        {},
+        {B, A},
+        {},
+        {D, C},
+        {}
+    };
+
+    // Ожидаемый результат: только непустые стеки
+    Node<int> nodeB{.count=1, .level=2, .next_nodes={}};
+    Node<int> nodeA{.count=1, .level=1, .next_nodes={{B, nodeB}}};
+
+    Node<int> nodeD{.count=1, .level=2, .next_nodes={}};
+    Node<int> nodeC{.count=1, .level=1, .next_nodes={{D, nodeD}}};
+
+    Node<int> expected{.count=2, .level=0, .next_nodes={{A, nodeA}, {C, nodeC}}};
+
+    auto actual = merge<int>(input);
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(merge, frame_stacks) {
+    // Тест на использование структуры Frame вместо int
+    auto input = std::vector<std::vector<Frame>>{
+        {Frame{"func2", "file2.cpp", 20, 10}, Frame{"func1", "file1.cpp", 10, 5}},
+        {Frame{"func3", "file3.cpp", 30, 15}, Frame{"func1", "file1.cpp", 10, 5}}
+    };
+
+    Node<Frame> nodeFunc2{.count=1, .level=2, .next_nodes={} };
+    Node<Frame> nodeFunc3{.count=1, .level=2, .next_nodes={} };
+    Node<Frame> nodeFunc1{.count=2, .level=1,
+        .next_nodes={{Frame{"func2", "file2.cpp", 20, 10}, nodeFunc2}, {Frame{"func3", "file3.cpp", 30, 15}, nodeFunc3}} };
+
+    Node<Frame> expected{.count=2, .level=0,
+        .next_nodes={{Frame{"func1", "file1.cpp", 10, 5}, nodeFunc1}}};
+
+    auto actual = merge<Frame>(input);
+    EXPECT_EQ(actual, expected);
+}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ threads-merger/merger.cppm
@@ -0,0 +1,135 @@
+module;
+
+#include <string>
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
+export module merger;
+
+export int foo()
+{
+    return 1;
+}
+
+export struct Frame
+{
+    std::string function;
+    std::string filename;
+    int row = 0;
+    int column = 0;
+
+    bool operator==(const Frame&) const = default;
+
+    // Оператор вывода для Frame
+    friend std::ostream& operator<<(std::ostream& os, const Frame& frame) {
+        os << "Frame{\"" << frame.function << "\", \"" << frame.filename << "\", "
+           << frame.row << ", " << frame.column << "}";
+        return os;
+    }
+};
+
+namespace std
+{
+
+export inline void hash_combine(std::size_t& seed, std::size_t hash) {
+    seed ^= hash + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+template<>
+struct hash<Frame> {
+    size_t operator()(const Frame& f) const noexcept {
+        size_t seed = 0;
+        hash_combine(seed, std::hash<std::string>{}(f.function));
+        hash_combine(seed, std::hash<std::string>{}(f.filename));
+        hash_combine(seed, std::hash<int>{}(f.row));
+        hash_combine(seed, std::hash<int>{}(f.column));
+        return seed;
+    }
+};
+
+} // namespace std
+
+export template<typename T>
+using Tree = std::unordered_map<T, std::unordered_set<T>>;
+
+export
+template<typename T>
+struct Node
+{
+    std::size_t count = 0;
+    std::size_t level = 0;
+    std::unordered_map<T, Node> next_nodes;
+
+    bool operator==(const Node&) const = default;
+
+    // Оператор вывода для Node
+    friend std::ostream& operator<<(std::ostream& os, const Node& node) {
+        return node.print_node(os, 0);
+    }
+
+private:
+    // Вспомогательная функция для рекурсивного вывода с отступами
+    std::ostream& print_node(std::ostream& os, int indent) const;
+};
+
+// Реализация функции print_node
+template<typename T>
+std::ostream& Node<T>::print_node(std::ostream& os, int indent) const {
+    const int MAX_DEPTH = 15; // Максимальная глубина рекурсии
+
+    if (this->level > MAX_DEPTH) {
+        os << "... (max depth exceeded)";
+        return os;
+    }
+
+    std::string indent_str(indent * 2, ' '); // 2 пробела на уровень отступа
+
+    os << indent_str << "Node{count=" << this->count << ", level=" << this->level;
+
+    if (!this->next_nodes.empty()) {
+        os << std::endl << indent_str << "  next_nodes:";
+        for (const auto& [value, next_node] : this->next_nodes) {
+            os << std::endl << indent_str << "    " << value << " ->";
+            next_node.print_node(os, indent + 2);
+        }
+    }
+
+    os << "}";
+    return os;
+}
+
+export
+template<typename T>
+Node<T> merge(const std::vector<std::vector<T>>& lists)
+{
+    Node<T> root{};
+
+    for (const auto& list: lists) {
+        if (list.empty()) continue;
+
+        root.count++; // Увеличиваем счетчик для каждого непустого стека
+
+        // Добавляем элементы в дерево, начиная с последнего (корневого)
+        Node<T>* current = &root;
+        for (std::size_t i = list.size(); i > 0; i--) {
+            const auto& value = list[i - 1];
+            std::size_t level = list.size() - i + 1; // Уровень: последний элемент = 1, предпоследний = 2, и т.д.
+
+            // Получаем ссылку на узел (создает новый, если не существует)
+            auto& node_ref = current->next_nodes[value];
+            if (node_ref.count == 0) {
+                // Новый узел
+                node_ref.count = 1;
+                node_ref.level = level;
+            } else {
+                // Существующий узел
+                node_ref.count++;
+            }
+            current = &node_ref;
+        }
+    }
+
+    return root;
+}
